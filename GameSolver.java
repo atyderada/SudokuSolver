efa7dc87fcda61784946fd3299cef879697ad99e
Anthony M. De Rada <anthonyderada@gmail.com>
2017-03-21 20:07:22 -0400

    Solution solves up to 16*16

--- GameSolver.java
+++ GameSolver.java
@@ -2,22 +2,6 @@
 public class GameSolver {
 	
 	private int boardSize;
-	public int getBoardSize() {
-		return boardSize;
-	}
-
-	public void setBoardSize(int boardSize) {
-		this.boardSize = boardSize;
-	}
-
-	public int[][] getBoardMatrix() {
-		return boardMatrix;
-	}
-
-	public void setBoardMatrix(int[][] boardMatrix) {
-		this.boardMatrix = boardMatrix;
-	}
-
 	private int[][] boardMatrix;
 	
 	public GameSolver(int[][] board, int size) {
@@ -35,7 +19,7 @@ public class GameSolver {
 	    for (int r = 0; r < this.boardMatrix.length; r++) {
 	        for (int c = 0; c < this.boardMatrix[0].length; c++) {
 	            if (this.boardMatrix[r][c] == 0) {
-	                for (int k = 1; k <= 9; k++) {
+	                for (int k = 1; k <= this.getBoardSize(); k++) {
 	                	this.boardMatrix[r][c] = k;
 	                    if (isValid(r, c) && solver()) {
 	                        return true;
@@ -52,9 +36,9 @@ public class GameSolver {
 
 	public boolean isValid(int r, int c) {
 	    //check row
-	    boolean[] row = new boolean[9];
-	    for (int i = 0; i < 9; i++) {
-	        if (this.boardMatrix[r][i] >= 1 && this.boardMatrix[r][i] <= 9) {
+	    boolean[] row = new boolean[this.getBoardSize()];
+	    for (int i = 0; i < this.getBoardSize(); i++) {
+	        if (this.boardMatrix[r][i] >= 1 && this.boardMatrix[r][i] <= this.getBoardSize()) {
 	            if (row[this.boardMatrix[r][i] - 1] == false) {
 	                row[this.boardMatrix[r][i] - 1] = true;
 	            } else {
@@ -64,9 +48,9 @@ public class GameSolver {
 	    }
 
 	    //check column
-	    boolean[] col = new boolean[9];
-	    for (int i = 0; i < 9; i++) {
-	        if (this.boardMatrix[i][c] >= 1 && this.boardMatrix[i][c] <= 9) {
+	    boolean[] col = new boolean[this.getBoardSize()];
+	    for (int i = 0; i < this.getBoardSize(); i++) {
+	        if (this.boardMatrix[i][c] >= 1 && this.boardMatrix[i][c] <= this.getBoardSize()) {
 	            if (col[this.boardMatrix[i][c] - 1] == false) {
 	                col[this.boardMatrix[i][c] - 1] = true;
 	            } else {
@@ -75,11 +59,12 @@ public class GameSolver {
 	        }
 	    }
 
-	    //check the 3*3 grid
-	    boolean[] grid = new boolean[9];
-	    for (int i = (r / 3) * 3; i < (r / 3) * 3 + 3; i++) {
-	        for (int j = (c / 3) * 3; j < (c / 3) * 3 + 3; j++) {
-	            if (this.boardMatrix[i][j] >= 1 && this.boardMatrix[i][j] <= 9) {
+	    //check the sqrt(size)*sqrt(size) grid
+	    boolean[] grid = new boolean[this.getBoardSize()];
+	    int factor = (int) Math.sqrt(this.getBoardSize());
+	    for (int i = (r / factor) * factor; i < (r / factor) * factor + factor; i++) {
+	        for (int j = (c / factor) * factor; j < (c / factor) * factor + factor; j++) {
+	            if (this.boardMatrix[i][j] >= 1 && this.boardMatrix[i][j] <= this.getBoardSize()) {
 	                if (grid[this.boardMatrix[i][j] - 1] == false) {
 	                    grid[this.boardMatrix[i][j] - 1] = true;
 	                } else {
@@ -90,5 +75,21 @@ public class GameSolver {
 	    }
 
 	    return true;
+	}
+	
+	public int getBoardSize() {
+		return boardSize;
+	}
+
+	public void setBoardSize(int boardSize) {
+		this.boardSize = boardSize;
+	}
+
+	public int[][] getBoardMatrix() {
+		return boardMatrix;
+	}
+
+	public void setBoardMatrix(int[][] boardMatrix) {
+		this.boardMatrix = boardMatrix;
 	}
 }
